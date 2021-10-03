755b5ccb258516efd9256699c9e89cc9f75a212e
unknown <cariitopandag@gmail.com>
2021-10-03 17:22:30 -0500

    CG Arreglo de la app general.

--- tenjs-tensalesapp/src/App.js
+++ tenjs-tensalesapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h2>TenSalesApp</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        <a class="App-link" href={"./login.html"}>Clic para continuar</a>
+        <a class="App-link" href={"./login/login.html"}>Clic para continuar</a>
         <div class="footer">
         <img class="logo10js" src="./images/logoTenJS.png" alt="Logo TenJS" />
         <p id="footer_p" ALIGN="middle">© 2021 TenJS. ALL RIGHTS RESERVED.</p>
